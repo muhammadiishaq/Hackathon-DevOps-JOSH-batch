41d766976b9efef9fe8a19cc24c55a06a21bea4c
Muhammad Ishaq <devopsaws804@gmail.com>
2026-05-17 06:06:40 +0500

    test/main: update workflow

---
+++ terraform/main.tf
@@ -0,0 +1,94 @@
+resource "aws_security_group" "Hackathon-sg" {
+  name = "Hackathon-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3100
+    to_port     = 3100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 32768
+    to_port     = 32768
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.Hackathon-sg.id
+  ]
+
+  user_data = file("${path.module}/user_data.sh")
+
+  root_block_device {
+    volume_size = 40
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Hackathon-DevSecOps"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "m7i-flex.large"
+}
+
+variable "key_name" {
+  type    = string
+  default = "hackathon"
+}
